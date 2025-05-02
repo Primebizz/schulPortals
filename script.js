b1b4e30daa5f8a7c111b21198d5448ebf41a1414
Primebizz <mubarakoloyede2@gmail.com>
2025-05-02 15:21:45 -0700

    FIrst Deployment

---
+++ script.js
@@ -0,0 +1,22 @@
+// 'use strict'
+
+// const header = document.querySelector('div#header');
+// const navBar = document.querySelector('div#navBar');
+// const body = document.body;
+
+// let lastKnownScrollPosition = 0;
+
+
+// body.addEventListener("scroll", (event) => {
+//     debugger
+//     lastKnownScrollPosition = window.scrollX
+
+//     if(lastKnownScrollPosition > '55'){
+//         navBar.style.display = 'none !important'
+//     }else{
+
+//     }
+// })
+
+// console.log(dynamicNav());
+
